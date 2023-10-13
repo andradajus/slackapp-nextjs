e0560c025a78db5d63cee8eaad5450fe03728ea7
Christine Joy Rosales <chrosales07@gmail.com>
2023-10-13 20:16:50 +0800

    Styled Welcome Page and created a new branch for succeeding edits

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,11 @@
-import Image from "next/image";
-import Link from "next/link";
+// import Image from "next/image";
+// import Link from "next/link";
 import WelcomePage from "./welcomepage/page";
 
 export default function ConversaPage() {
   return (
     <>
       <WelcomePage />
-      <header>Conversa</header>
-      <p>Your Communication Friend Online</p>
-
-      <footer>
-        <h6>2023 Conversa</h6>
-      </footer>
     </>
   );
 }
--- src/app/welcomepage/page.tsx
+++ src/app/welcomepage/page.tsx
@@ -1,26 +1,51 @@
 import React from "react";
 import Link from "next/link";
+import Image from "next/image";
 
 export default function WelcomePage() {
   return (
-    <>
-      <main>
-        <h1>Welcome to Conversa</h1>
-        <p>Your Communication Friend Online</p>
+    <div className="min-h-screen flex flex-col items-center justify-center text-center bg-indigo-900 text-white">
+      <main className="m-0 p-5">
+        <h1 className="text-5xl font-bold mb-0 text-yellow-400 font-serif">
+          Conversa
+        </h1>
+        <p className="text-s mb-4 italic">Your Communication Friend Online</p>
       </main>
-      <section>
-        <h4>How can we help you today?</h4>
+      <section className="font-sans">
+        <Image
+          src="/ConversaImage.png"
+          alt="ConversaImage"
+          width={150}
+          height={150}
+          className="mb-6 mx-auto"
+        />
+
+        <h4 className="text-lg">How can we help you today?</h4>
         <nav>
-          <ul>
-            <li>
-              <Link href="/login">Log me in</Link>
+          <ul className="mt-4 inline-flex text-base text-yellow-200">
+            <li className="mb-2 mr-8">
+              <Link
+                href="/login"
+                className="hover:text-orange-300 hover:underline"
+              >
+                Log me in
+              </Link>
             </li>
             <li>
-              <Link href="/signup">I wanna register</Link>
+              <Link
+                href="/signup"
+                className="hover:text-orange-300 hover:underline"
+              >
+                I wanna register
+              </Link>
             </li>
           </ul>
         </nav>
       </section>
-    </>
+
+      <footer className="mt-8">
+        <h6 className="text-xs">&copy; 2023 Conversa</h6>
+      </footer>
+    </div>
   );
 }
