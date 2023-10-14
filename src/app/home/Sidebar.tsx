9394bd959145b0f2e3ecd99dadef806bdc79c42b
andradajus <andradajustinmarcus@gmail.com>
2023-10-14 10:43:01 +0800

    Added styling on message input and namemodal

---
+++ src/app/home/Sidebar.tsx
@@ -0,0 +1,17 @@
+const Sidebar = () => {
+return (
+    <>
+    <div className="flex flex-col bg-indigo-500">
+<div>Logo</div>
+<div>My Account</div>
+<div>DMs</div>
+<div>Channels</div>
+<div>Logout</div>
+</div>
+
+  </>
+
+  )
+}
+
+  export default Sidebar
